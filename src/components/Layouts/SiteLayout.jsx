6fd30cdbd0a593c6117d063b826a23c8980c3ce3
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-04-30 18:33:54 +0300

    feat(assets): add English and Arabic SVG icons for layout page and icons to login page

--- src/components/Layouts/SiteLayout.jsx
+++ src/components/Layouts/SiteLayout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
+import EN from "../../assets/siteLang/EN.svg";
+import AR from "../../assets/siteLang/AR.svg";
 
 const SiteLayout = () => {
   const { i18n } = useTranslation();
@@ -18,9 +20,22 @@ const SiteLayout = () => {
   };
   return (
     <>
-      <header className="bg-white flex container ">
-        <button onClick={handleLanguageChange}>
-          {i18n.language === "en" ? "AR" : "EN"}
+      <header className="bg-white flex container mb-[64px]">
+        <button
+          onClick={handleLanguageChange}
+          className="bg-transparent text-black px-3 py-1 rounded-3xl absolute top-4 left-4 border-1 border-secondary-light cursor-pointer flex items-center gap-2"
+        >
+          {i18n.language === "en" ? (
+            <div className="flag flex items-center gap-2">
+              <img src={EN} alt="English Flag" className="w-3 h-3" />
+              <p>EN</p>
+            </div>
+          ) : (
+            <div className="flag flex items-center gap-2">
+              <img src={AR} alt="Arabic Flag" className="w-3 h-3 " />
+              <p>AR</p>
+            </div>
+          )}
         </button>
       </header>
       <Outlet />
